9ce8c605645e7cdc76fc74ff3e8720e229859323
fansigning <freemeowfr@gmail.com>
2025-04-16 11:30:48 +0200

    Update dc.js

--- dc.js
+++ dc.js
@@ -1,32 +1,17 @@
+const pfp = document.getElementById('discord-pfp');
+const name = document.getElementById('discord-name');
+
+// Set placeholder first
+pfp.src = 'https://placehold.co/150?text=%3C3';
+
+// Fetch Discord info
 fetch('https://stash.seraph.si/discord/user/1239374720693178398')
     .then(res => res.json())
     .then(data => {
-        // Discord profile picture and name
-        document.getElementById('discord-pfp').src = data.extra.avatar_url;
-        document.getElementById('discord-name').textContent = data.user.global_name || data.user.username;
-
-        setTimeout(() => {
-            const overlay = document.getElementById('overlay');
-            overlay.classList.replace('bg-black', 'bg-black/80');
-        }, 100); 
+        pfp.src = data.extra.avatar_url;
+        name.textContent = data.user.global_name || data.user.username;
     })
     .catch(err => {
         console.error('Error fetching Discord data:', err);
-        document.getElementById('discord-name').textContent = 'failed to load';
+        name.textContent = 'failed to load';
     });
-
-// overlay
-overlay.addEventListener('click', () => {
-    audio.load(); // ios fix
-    audio.play().then(() => {
-      playBtn.classList.replace('mdi-play', 'mdi-pause');
-      overlay.classList.replace('opacity-100', 'opacity-0');
-      overlay.style.pointerEvents = 'none';
-      setTimeout(() => {
-        overlay.style.display = 'none';
-      }, 700);
-    }).catch((error) => {
-      console.error("Autoplay was blocked:", error);
-    });
-  });
-  

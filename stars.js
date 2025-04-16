7996735f31881869c0914e88cd4b636b02033cc6
fansigning <freemeowfr@gmail.com>
2025-04-16 15:03:06 +0200

    Update stars.js

--- stars.js
+++ stars.js
@@ -1,28 +1,32 @@
 function generateStars(num) {
   const body = document.querySelector("body");
 
-  const stars = window.innerWidth <= 768 ? Math.floor(num / 2) : num; // less stars if small screen
+  // smaller screens
+  const stars = window.innerWidth <= 768 ? Math.floor(num / 2) : num;
 
   for (let i = 0; i < stars; i++) {
     const star = document.createElement("div");
     const size = Math.random() * 3 + 1; // random size
-    const posX = Math.random() * window.innerWidth; // random x
-    const posY = Math.random() * window.innerHeight; // random y
-    const randomX = Math.random() * 200 - 100; // random horizontal move
-    const randomY = Math.random() * 200 - 100; // random vertical move
+    const posX = Math.random() * (window.innerWidth - size); // random x
+    const posY = Math.random() * (window.innerHeight - size); // random y
+    const randomX = Math.random() * 50 - 25; 
+    const randomY = Math.random() * 50 - 25; 
     const animationDelay = Math.random() * 5 + "s"; // random delay
 
     star.style.setProperty("--randomX", `${randomX}px`);
     star.style.setProperty("--randomY", `${randomY}px`);
     star.classList.add("star");
-    star.classlist.add("z-60");
+    star.classList.add('z-70');
     star.style.width = `${size}px`;
     star.style.height = `${size}px`;
     star.style.left = `${posX}px`;
     star.style.top = `${posY}px`;
     star.style.animationDelay = animationDelay;
+
     body.appendChild(star);
   }
 }
 
 generateStars(150);
+
+document.body.style.overflow = "hidden";  
